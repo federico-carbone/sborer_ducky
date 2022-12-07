17151c5e385b06f776ea36a5a4dc8f286b727ca0
Federico Carbone <43215754+federico-carbone@users.noreply.github.com>
2022-12-07 12:22:09 +0100

    Initial commit

---
+++ string_to_struct.py
@@ -0,0 +1,12 @@
+s = "https://www.youtube.com/watch?v=dQw4w9WgXcQ\n"
+
+m = "abcdefghijklmnopqrstuvwxyz1234567890\nù\b\t -ù[]\\#;'ù,.ùù"
+M = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"£$%&/()=ùùùùù?*\{\}|~ù\"ù;:ù"
+
+for c in s:
+    if c in m:
+        print("\t{", "0,0,{0},0,0,0,0,0".format(hex(m.index(c)+4)), "},", sep="")
+    elif c in M:
+        print("\t{", "0x2,0,{0},0,0,0,0,0".format(hex(M.index(c)+4)), "},", sep="")
+    else:
+        print("error on character {0}".format(c))
